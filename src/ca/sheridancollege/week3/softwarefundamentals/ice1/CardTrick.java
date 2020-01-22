1cbdc49a3a7b021aba720d3e5ac994d5ac9e52e8
GursikhHajra <hajra@sheridancollege.ca>
2020-01-22 11:17:07 -0500

    2st commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,21 +11,37 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author 
  */
+import java.util.Scanner;
+
 public class CardTrick {
     
     public static void main(String[] args)
     {
+         Scanner sc = new Scanner(System.in);
+
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * 13));
+           // c.setSuit(int(Card.SUITS((Math.random() * 4)));
         }
         
         //insert code to ask the user for Card value and suit, create their card
+                System.out.println("Enter the card number");
+            int n = sc.nextInt();
+       System.out.println("Enter the suit number");
+            int s = sc.nextInt();
+            
         // and search magicHand here
+           for (int q=0;q<magicHand.length;q++)
+           {
+           //    if (magicHand[] == n && s)
+                 System.out.println("Win");  
+               
+                   
+           }
         //Then report the result here
     }
     
