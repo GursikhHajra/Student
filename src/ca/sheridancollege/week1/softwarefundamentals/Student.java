b9bf2ce25ac654d3bb42627f3dda14653abdb464
GursikhHajra <hajra@sheridancollege.ca>
2020-01-22 09:56:37 -0500

    2st commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
-    private String GPA;
+    private String hu;
 
     /**
      * Get the value of name
